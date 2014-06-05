a9315ef0530365cab571f3651a1ac570db02e0fc
Bernie Keim <bernie@q2consulting.ca>
2014-06-05 13:21:55 -0400

    New plot 3 and plot4.R

--- plot4.R
+++ plot4.R
@@ -15,7 +15,7 @@ subData$Voltage <- as.numeric(subData$Voltage)
 subData$Global_reactive_power <- as.numeric(subData$Global_reactive_power)
 
 ## Set number of graphs and set margins
-par(mfrow = c(2,2), mar = c(4, 4.1, 2, 2))
+par(mfrow = c(2,2), mar = c(4, 4, 2, 2))
 ## Plot globabl active power
 with(subData, plot(DateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
 
@@ -26,12 +26,13 @@ with(subData, plot(DateTime, Voltage, type = "l", ylab = "Voltage"))
 with(subData, plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab="Energy sub metering"))
 with(subData, lines(DateTime, Sub_metering_2, col = "red"))
 with(subData, lines(DateTime, Sub_metering_3, col = "blue"))
-legend("topright", lty = 1, col = c("black", "red", "blue"), cex = .75, bty = "n", 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), cex = .75,  
+       bty = "n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Plot global reactive power
 with(subData, plot(DateTime, Global_reactive_power, type = "l"))
 
 ## Write the plot to a png file
-dev.copy(png, file = "plot4.png")
+dev.copy(png, file = "plot4.png", type = "windows")
 dev.off()
+
